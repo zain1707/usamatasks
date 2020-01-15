e73a5ef005c201a79c03966380b8fdc387155aa1
zainaamir93 <zainaamir93@gmail.com>
2020-01-16 00:06:35 +0500

    Dev Branch Testing

--- src/api/server.go
+++ src/api/server.go
@@ -11,12 +11,17 @@ func yallo(c echo.Context) error {
 	return c.JSON(http.StatusOK, "yallo from the web side!")
 }
 
+func test(c echo.Context) error {
+	return c.JSON(http.StatusOK, "testing")
+}
+
 func main() {
 	fmt.Println("Welcome to the server")
 
 	e := echo.New()
 
 	e.GET("/", yallo)
+	e.GET("/test", yallo)
 
 	e.Start(":8000")
 }
